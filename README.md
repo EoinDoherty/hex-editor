235b238a28c809f34387b8facdbbf0d8988cc87b
Eoin Doherty <eoin@engr2-1-7-153-edu.int.colorado.edu>
2017-03-11 00:58:04 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# hex-editor package
+
+A short description of your package.
+Converts a file to hex and converts that hex dump back into the original file format.
+Keyboard shortcut: ctrl-shift-h
---
+++ spec/hex-editor-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import HexEditorView from '../lib/hex-editor-view';
+
+describe('HexEditorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});

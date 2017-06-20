e5c416ca409e6e5d020a0a54ea2ec4a956c237d4
Eoin Doherty <EoinDoherty@users.noreply.github.com>
2017-06-19 21:18:17 -0600

    Cleaned up

--- spec/hex-editor-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import HexEditorView from '../lib/hex-editor-view';
-
-describe('HexEditorView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
